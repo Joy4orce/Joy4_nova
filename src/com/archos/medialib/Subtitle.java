171cdedf4ae29a5e9e0591a5d6ca0291dc4b8c0d
Courville Software <software@courville.org>
2024-11-09 15:41:58 +0100

    pgs subs needs more jni magic

--- src/com/archos/medialib/Subtitle.java
+++ src/com/archos/medialib/Subtitle.java
@@ -17,7 +17,13 @@ package com.archos.medialib;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class Subtitle {
+
+    private static final Logger log = LoggerFactory.getLogger(Subtitle.class);
+
     private final int type;
     private static final int TYPE_TEXT_SUBTITLE = 1;
     private static final int TYPE_TIMED_TEXT_SUBTITLE = 2;
@@ -44,6 +50,9 @@ public abstract class Subtitle {
     public abstract Rect getBounds();
     public abstract int getPosition();
     public abstract int getDuration();
+    public abstract void setDuration(int duration);
+    public abstract int getFrameWidth();
+    public abstract int getFrameHeight();
 
     public static class TextSubtitle extends Subtitle {
         private final String text;
@@ -55,8 +64,12 @@ public abstract class Subtitle {
         public String getText() {
             return this.text;
         }
-        public Bitmap getBitmap() {
-            return null;
+        public Bitmap getBitmap() { return null; }
+        public int getFrameWidth() {
+            return 0;
+        }
+        public int getFrameHeight() {
+            return 0;
         }
         public Rect getBounds() {
             return null;
@@ -67,11 +80,12 @@ public abstract class Subtitle {
         public int getDuration() {
             return -1;
         }
+        public void setDuration(int duration) {}
     }
 
     public abstract static class Timed extends Subtitle {
         private final int position;
-        private final int duration;
+        private int duration; // not final because sometimes duration is modified by empty next subtitle
 
         public Timed(int type, int position, int duration) {
             super(type);
@@ -85,6 +99,7 @@ public abstract class Subtitle {
         public int getDuration() {
             return this.duration;
         }
+        public void setDuration(int duration) { this.duration = duration; }
     }
 
     public static class TimedTextSubtitle extends Timed {
@@ -101,6 +116,12 @@ public abstract class Subtitle {
         public Bitmap getBitmap() {
             return null;
         }
+        public int getFrameWidth() {
+            return 0;
+        }
+        public int getFrameHeight() {
+            return 0;
+        }
         public Rect getBounds() {
             return null;
         }
@@ -109,11 +130,16 @@ public abstract class Subtitle {
     public static class TimedBitmapSubtitle extends Timed {
         private final Bitmap bitmap;
         private final Rect bounds;
+        private final int frameWidth;
+        private final int frameHeight;
 
-        public TimedBitmapSubtitle(int position, int duration, int originalWidth, int originalHeight, Bitmap bitmap) {
+        public TimedBitmapSubtitle(int position, int duration, int leftCorner, int topCorner, int originalWidth, int originalHeight, Bitmap bitmap) {
             super(TYPE_TIMED_BITMAP_SUBTITLE, position, duration);
             this.bitmap = bitmap;
-            this.bounds = new Rect(0, 0, originalWidth, originalHeight);
+            this.frameWidth = originalWidth;
+            this.frameHeight = originalHeight;
+            this.bounds = new Rect(leftCorner, topCorner, leftCorner + bitmap.getWidth(), topCorner + bitmap.getHeight());
+            log.debug("TimedBitmapSubtitle: position={}, duration={}, framewidth={}, frameheight={}, bounds={}", position, duration, frameWidth, frameHeight, bounds);
         }
         public String getText() {
             return null;
@@ -122,15 +148,18 @@ public abstract class Subtitle {
             return this.bitmap;
         }
         public Rect getBounds() {
-            return bounds;
+            return this.bounds;
         }
+        public int getFrameWidth() { return this.frameWidth; }
+        public int getFrameHeight() { return this.frameHeight; }
     }
 
     // from jni
     public static Object createTimedTextSubtitle(int position, int duration, String text) {
         return new Subtitle.TimedTextSubtitle(position, duration, text);
     }
-    public static Object createTimedBitmapSubtitle(int position, int duration, int originalWidth, int originalHeight, Bitmap bitmap) {
-        return new Subtitle.TimedBitmapSubtitle(position, duration, originalWidth, originalHeight, bitmap);
+
+    public static Object createTimedBitmapSubtitle(int position, int duration, int leftCorner, int topCorner, int originalWidth, int originalHeight, Bitmap bitmap) {
+        return new Subtitle.TimedBitmapSubtitle(position, duration, leftCorner, topCorner, originalWidth, originalHeight, bitmap);
     }
 }
