f841c48cd7e2ab72a7a49a39e42c0fa16ecfa7fd
Courville Software <software@courville.org>
2024-12-16 19:09:26 +0100

    remove not used imports

--- src/com/archos/mediaprovider/video/NetworkScannerUtil.java
+++ src/com/archos/mediaprovider/video/NetworkScannerUtil.java
@@ -14,23 +14,17 @@
 
 package com.archos.mediaprovider.video;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Build;
-import android.util.Log;
 
 import androidx.preference.PreferenceManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
