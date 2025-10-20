293ecfddc23627b5eae35c9d253484f596de8561
Courville Software <software@courville.org>
2025-10-20 18:55:39 +0200

    DelayedBackgroundLoader: logback

--- src/main/java/com/archos/mediacenter/video/utils/DelayedBackgroundLoader.java
+++ src/main/java/com/archos/mediacenter/video/utils/DelayedBackgroundLoader.java
@@ -17,7 +17,6 @@ package com.archos.mediacenter.video.utils;
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.archos.mediacenter.utils.imageview.ImageProcessor;
@@ -25,10 +24,12 @@ import com.archos.mediacenter.utils.imageview.LoadTaskItem;
 import com.archos.mediacenter.utils.imageview.LoadResult.Status;
 import com.archos.mediascraper.ScraperImage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DelayedBackgroundLoader extends ImageProcessor {
 
-    protected static final String TAG = "DelayedBackgroundLoader";
-    protected static final boolean DBG = false;
+    private static final Logger log = LoggerFactory.getLogger(DelayedBackgroundLoader.class);
 
     private final Context mContext;
     private final long mSleep;
@@ -43,7 +44,7 @@ public class DelayedBackgroundLoader extends ImageProcessor {
 
     @Override
     public void loadBitmap(LoadTaskItem taskItem) {
-        if (DBG) Log.d(TAG, "background:loadBitmap");
+        log.debug("background:loadBitmap");
         long time = System.currentTimeMillis();
         if (taskItem.loadObject instanceof ScraperImage) {
             ScraperImage image = (ScraperImage) taskItem.loadObject;
@@ -52,19 +53,19 @@ public class DelayedBackgroundLoader extends ImageProcessor {
                 image.download(mContext);
                 try {
                     taskItem.result.bitmap = BitmapFactory.decodeFile(file);
-                    if (DBG) Log.d(TAG, "background:loadBitmap - loaded.");
+                    log.debug("background:loadBitmap - loaded.");
                 } catch (OutOfMemoryError e) {
-                    Log.e(TAG, "background:loadBitmap - No more memory!", e);
+                    log.error("background:loadBitmap - No more memory!", e);
                 }
 
                 boolean needSleep = !file.equals(lastLoaded);
                 if (needSleep && taskItem.result.bitmap != null) {
                     long sleep = mSleep - (System.currentTimeMillis() - time);
                     if (sleep > 0) try {
-                        if (DBG) Log.d(TAG, "background:loadBitmap - extra sleep of " + sleep);
+                        log.debug("background:loadBitmap - extra sleep of {}", sleep);
                         Thread.sleep(sleep);
                     } catch (InterruptedException e) {
-                        if (DBG) Log.d(TAG, "DelayedBackgroundLoader interrupted in sleep()");
+                        log.debug("DelayedBackgroundLoader interrupted in sleep()");
                     }
                 }
             }
@@ -94,32 +95,32 @@ public class DelayedBackgroundLoader extends ImageProcessor {
 
     @Override
     public void setResult(ImageView imageView, LoadTaskItem taskItem) {
-        if (DBG) Log.d(TAG, "setResult");
+        log.debug("setResult");
         boolean animate = true;
         if (taskItem.loadObject instanceof ScraperImage) {
             ScraperImage image = (ScraperImage) taskItem.loadObject;
             String file = image.getLargeFile();
             if (file != null && file.equals(lastLoaded)) {
                 //!!!animate = false;
-                if (DBG) Log.d(TAG, "setResult animate false");
+                log.debug("setResult animate false");
             }
             lastLoaded = file;
         }
 
         imageView.setImageBitmap(taskItem.result.bitmap);
         if (animate) {
-            Log.d(TAG, "   animate backdrop alpha "+mBackgroundAlpha);
+            log.debug("   animate backdrop alpha {}", mBackgroundAlpha);
             imageView.animate().alpha(mBackgroundAlpha).setDuration(600);
         }
         else {
-            Log.d(TAG, "   set backdrop alpha"+mBackgroundAlpha);
+            log.debug("   set backdrop alpha {}", mBackgroundAlpha);
             imageView.setAlpha(mBackgroundAlpha);
         }
     }
 
     @Override
     public boolean handleLoadError(ImageView imageView, LoadTaskItem taskItem) {
-        if (DBG) Log.d(TAG, "set Null animated");
+        log.debug("set Null animated");
         if (imageView.getAlpha() > 0f)
             imageView.animate().alpha(0f).setDuration(300);
         lastLoaded = null;
