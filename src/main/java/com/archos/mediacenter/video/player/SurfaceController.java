Joy4orce/Joy4_nova

commit 3c1bf8c15447fc7de1ffd8e4ab61848bbe201c69

edge to edge sdk 35: refactor code to manage cutout and system bars
enable cutout avoidance in surfaceView (videoView)

center subtitleView inside videoView and avoid text clipping in presence of round screen edges

shift subtitleTextView to not overlap playerController bars

src/main/java/com/archos/mediacenter/video/player/SurfaceController.java
@@ -18,9 +18,6 @@ import com.archos.mediacenter.video.R;
 
 import com.archos.medialib.IMediaPlayer;
 
-import android.graphics.SurfaceTexture;
-import android.opengl.GLSurfaceView;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.TextureView;
@@ -106,6 +103,11 @@ public class SurfaceController {
     private int mEffectMode = VideoEffect.getDefaultMode();
     private int mEffectType = VideoEffect.getDefaultType();
 
+    private int mCutoutLeft = 0;
+    private int mCutoutTop = 0;
+    private int mCutoutRight = 0;
+    private int mCutoutBottom = 0;
+
     public SurfaceController(View rootView) {
         ViewGroup mLp = (ViewGroup)rootView;
  
@@ -121,7 +123,7 @@ public class SurfaceController {
     }
   
     public void setGLSupportEnabled(boolean enable){
-        log.debug("setGLSupportEnabled: " + enable);
+        log.debug("setGLSupportEnabled: {}", enable);
         if (mEffectEnable == enable) return;
         mView.setVisibility(View.GONE);
         if (enable) {
@@ -132,7 +134,7 @@ public class SurfaceController {
              mView = mSurfaceView;
         }
         mView.setVisibility(View.VISIBLE);
-    	mEffectEnable = enable;
+        mEffectEnable = enable;
     	updateSurface();
     }
     synchronized public void setMediaPlayer(IMediaPlayer player) {
@@ -146,7 +148,7 @@ public class SurfaceController {
     }
     
     public boolean supportOpenGLVideoEffect() {
-        log.debug("supportOpenGLVideoEffect: " + ((mEffectView == mView) && (VideoEffect.openGLRequested(mEffectType))));
+        log.debug("supportOpenGLVideoEffect: {}", (mEffectView == mView) && (VideoEffect.openGLRequested(mEffectType)));
         return (mEffectView == mView) && (VideoEffect.openGLRequested(mEffectType));
     }
 
@@ -156,7 +158,7 @@ public class SurfaceController {
     }
 
     public void setHdmiPlugged(boolean plugged, int hdmiWidth, int hdmiHeight) {
-        log.debug("setHdmiPlugged: plugged=" + plugged + ", hdmiWidth=" + hdmiWidth + ", hdmiHeight=" + hdmiHeight);
+        log.debug("setHdmiPlugged: plugged={}, hdmi=({},{})", plugged, hdmiWidth, hdmiHeight);
         if (plugged != mHdmiPlugged) {
             mHdmiPlugged = plugged;
             mHdmiWidth = hdmiWidth;
@@ -169,6 +171,16 @@ public class SurfaceController {
         mLcdWidth = lcdWidth;
         mLcdHeight = lcdHeight;
         updateSurface();
+    }
+
+    public void setCutoutMetrics(int cutoutLeft, int cutoutTop, int cutoutRight, int cutoutBottom) {
+        if (mCutoutLeft != cutoutLeft || mCutoutTop != cutoutTop || mCutoutRight != cutoutRight || mCutoutBottom != cutoutBottom) {
+            mCutoutLeft = cutoutLeft;
+            mCutoutTop = cutoutTop;
+            mCutoutRight = cutoutRight;
+            mCutoutBottom = cutoutBottom;
+            updateSurface();
+        }
     }
 
     public void setVideoSize(int videoWidth, int videoHeight, double aspect) {
@@ -187,7 +199,7 @@ public class SurfaceController {
         return getVideoFormat().getMax();
     }
     public int getCurrentFormat(){
-        log.debug("getCurrentFormat: " + getVideoFormat().getFmt());
+        log.debug("getCurrentFormat: {}", getVideoFormat().getFmt());
         return getVideoFormat().getFmt();
     }
     private VideoFormat getVideoFormat() {
@@ -218,7 +230,7 @@ public class SurfaceController {
         }
     }
     public void setVideoFormat(int fmt, int autoFmt) {
-        log.debug("setVideoFormat fmt=" + fmt + ", autoFmt=" + autoFmt);
+        log.debug("setVideoFormat fmt={}, autoFmt={}", fmt, autoFmt);
         mVideoFormat.setFmt(fmt);
         mAutoVideoFormat.setFmt(autoFmt);
         updateSurface();
@@ -249,19 +261,23 @@ public class SurfaceController {
         if (mHdmiPlugged) {
             dw = mHdmiWidth;
             dh = mHdmiHeight;
-            log.debug("updateSurface: hdmi plugged dw=" + dw + ", dh=" + dh);
+            log.debug("CONFIG updateSurface: hdmi plugged d=({},{})", dw, dh);
         } else {
             dw = mLcdWidth;
             dh = mLcdHeight;
-            log.debug("updateSurface: lcd plugged dw=" + dw + ", dh=" + dh);
+            log.debug("CONFIG updateSurface: lcd plugged d=({},{})", dw, dh);
         }
 
+        // display width and height without cutout
+        int dcw = dw - mCutoutLeft - mCutoutRight;
+        int dch = dh - mCutoutTop - mCutoutBottom;
         vw = mVideoWidth;
         vh = mVideoHeight;
-        log.debug("updateSurface: vw=" + vw + ", vh=" + vh);
+
+        log.debug("CONFIG updateSurface: v=({},{})", vw, vh);
 
         if (mMediaPlayer == null) log.warn("updateSurface: mMediaPlayer is null!");
-        if (vw <= 0 || vh <= 0 || dw <= 0 || dh <= 0 || mMediaPlayer == null)
+        if (vw <= 0 || vh <= 0 || dcw <= 0 || dch <= 0 || mMediaPlayer == null)
             return;
         fmt = getVideoFormat().getFmt();
 
@@ -270,88 +286,84 @@ public class SurfaceController {
         }
 
         // calculate aspect ratio
-        double sar = (double) vw / (double) vh;
-        double ar = par * sar;
-        // calculate display aspect ratio
-        double dar = (double) dw / (double) dh;
+        double sar = (double) vw / (double) vh; // sar = source aspect ratio (video)
+        double ar = par * sar; // ar = aspect ratio of the video
+        double dar = (double) dw / (double) dh; // display aspect ratio
+        double dcar = (double) dcw / (double) dch; // display aspect ratio without cutout
 
-        log.debug("updateSurface: sar=" + sar + ", ar=" + ar + ", dar=" + dar);
+        log.debug("CONFIG updateSurface: sar={}, ar={}, dar={}, dcar={}", sar, ar, dar, dcar);
 
         cropW = cropH = 1.0f;
         switch (fmt) {
             case VideoFormat.ORIGINAL:
-                if (dar < ar) {
-                    //4:3 movie on 16:9 screen
-                    dh = (int) (dw/ (ar));
-                    log.debug("updateSurface: VideoFormat.ORIGINAL dar<ar dh=" + dh);
+                if (dcar < ar) {
+                    //4:3 movie on 16:9 screen or 16:9 movie on portrait screen
+                    dch = (int) (dcw/ (ar));
+                    log.debug("CONFIG updateSurface: VideoFormat.ORIGINAL dcar<ar dch={}", dch);
                 } else {
                     //16:9 movie on 4:3 screen
-                    dw = (int) (dh * ar);
-                    log.debug("updateSurface: VideoFormat.ORIGINAL dar>=ar dw=" + dw);
+                    dcw = (int) (dch * ar);
+                    log.debug("CONFIG updateSurface: VideoFormat.ORIGINAL dcar>=ar dcw={}", dcw);
                 }
                 break;
             case VideoFormat.FULLSCREEN:
-                if (dar < ar) {
+                if (dcar < ar) {
                     //4:3 movie on 16:9 screen
-                    cropW = (float)dar / (float)ar;
+                    cropW = (float)dcar / (float)ar;
                     cropH = 1.0f;
-                    log.debug("updateSurface: VideoFormat.FULLSCREEN dar<ar 4:3 movie on 16:9 screen dw=" + dw + ", dh=" + dh + ", cropW=" + cropW + ", cropH=" + cropH);
+                    log.debug("CONFIG updateSurface: VideoFormat.FULLSCREEN dcar<ar 4:3 movie on 16:9 screen dc=({},{}), crop=({},{})", dcw, dch, cropW, cropH);
                 } else {
                     //16:9 movie on 4:3 screen
-                    cropH = (float)ar / (float)dar;
+                    cropH = (float)ar / (float)dcar;
                     cropW = 1.0f;
-                    log.debug("updateSurface: VideoFormat.FULLSCREEN dar>=ar 16:9 movie on 4:3 screen dw=" + dw + ", dh=" + dh + ", cropW=" + cropW + ", cropH=" + cropH);
+                    log.debug("CONFIG updateSurface: VideoFormat.FULLSCREEN dcar>=ar 16:9 movie on 4:3 screen dc=({},{}), crop=({},{})", dcw, dch, cropW, cropH);
                 }
                 break;
             case VideoFormat.AUTO: {
                 cropW = 1.0f;
                 cropH = 1.0f;
-                if (dar > ar) {
-                    dw = dw + (((int) (dh * ar)) - dw) / 2;
-                    cropH = (float) dh / (float) (dw / ar);
-                    log.debug("updateSurface: VideoFormat.AUTO dar>ar dw=" + dw + ", dh=" + dh);
+                if (dcar > ar) {
+                    dcw = dcw + (((int) (dch * ar)) - dcw) / 2;
+                    cropH = (float) dch / (float) (dcw / ar);
+                    log.debug("CONFIG updateSurface: VideoFormat.AUTO dcar>ar dc=({},{})", dcw, dch);
                 } else {
-                    dh = dh + (((int) (dw / ar)) - dh) / 2;
-                    cropW = (float) dw / (float) (dh * ar);
-                    log.debug("updateSurface: VideoFormat.AUTO dar<=ar dw=" + dw + ", dh=" + dh);
+                    dch = dch + (((int) (dcw / ar)) - dch) / 2;
+                    cropW = (float) dcw / (float) (dch * ar);
+                    log.debug("CONFIG updateSurface: VideoFormat.AUTO dcar<=ar dc=({},{})", dcw, dch);
                 }
                 break;
             }
-            case VideoFormat.STRETCHED: { // display on full screen resolution stretched
+            case VideoFormat.STRETCHED: { // display on full screen resolution stretched: keep dcw and dch
                 cropW = 1.0f;
                 cropH = 1.0f;
-                // this is the original size no zoom/stretch
-                //dw = vw;
-                //dh = vh;
-                log.debug("updateSurface: VideoFormat.STRETCHED dw=" + dw + ", dh=" + dh + ", cropW=" + cropW + ", cropH=" + cropH);
+                log.debug("CONFIG updateSurface: VideoFormat.STRETCHED dc=({},{}), crop=({},{})", dcw, dch, cropW, cropH);
                 break;
             }
         }
 
-        if (((mEffectMode & VideoEffect.TB_MODE)!=0) && (ar <= 1.5)) {
-            dw *= 2;
-        }
-        if (((mEffectMode & VideoEffect.SBS_MODE)!=0) && (ar >= 3.0)) {
-            dh *= 2;
-        }
+        if (((mEffectMode & VideoEffect.TB_MODE)!=0) && (ar <= 1.5)) dcw *= 2;
+        if (((mEffectMode & VideoEffect.SBS_MODE)!=0) && (ar >= 3.0)) dch *= 2;
 
-        log.debug("updateSurface: setFixedSize(" + vw + "," + vh + ")");
+        log.debug("CONFIG updateSurface: setFixedSize({},{})", vw, vh);
 
-        if (mSurfaceView != null)
-            mSurfaceView.getHolder().setFixedSize(vw, vh);
+        if (mSurfaceView != null) mSurfaceView.getHolder().setFixedSize(vw, vh);
 
-        dw = Math.round(dw  / cropW);
-        dh = Math.round(dh / cropH);
+        dcw = Math.round(dcw  / cropW);
+        dch = Math.round(dch / cropH);
+
+        log.debug("CONFIG updateSurface: setLayoutParams({},{})", dcw, dch);
+
+        // set the view size to display size but keep the cutout margins
         ViewGroup.LayoutParams lp = mView.getLayoutParams();
-        lp.width = dw;
-        lp.height = dh;
-
-        log.debug("updateSurface: setLayoutParams(" + dw + "," + dh + ")");
-
+        lp.width = dcw;
+        lp.height = dch;
+        // Note: do not set margins of mCutoutLeft, mCutoutTop, mCutoutRight, mCutoutBottom here, delegate to PlayerActivity
         mView.setLayoutParams(lp);
-
         mView.invalidate();
 
-        log.debug("updateSurface: " + vw + "x" + vh + " -> " + dw + "x" + dh + " / formatCrop: " + cropW + "x" + cropH + " / mEffectMode: "+mEffectMode);
+        log.debug("CONFIG updateSurface: ({},{})->({},{}) / formatCrop: ({},{}) / mEffectMode: {}", vw, vh, dcw, dch, cropW, cropH, mEffectMode);
     }
+
+    public int getViewHeight() { return mView.getHeight(); }
+    public int getViewWidth() { return mView.getWidth(); }
 }

src/main/java/com/archos/mediacenter/video/player/Player.java
@@ -1201,6 +1201,7 @@ public class Player implements IPlayerControl,
     public int getVideoHeight(){
         return mVideoHeight;
     }
+    public double getVideoAspect() { return mVideoAspect; }
     public void onVideoSizeChanged(IMediaPlayer mp, int width, int height) {
         mVideoWidth = width;
         mVideoHeight = height;

src/main/java/com/archos/mediacenter/video/player/PlayerController.java
@@ -22,11 +22,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
@@ -41,23 +37,17 @@ import androidx.appcompat.app.ActionBar;
 import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
 import android.view.Display;
-import android.view.DisplayCutout;
 import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.Surface;
 import android.view.View;
-import android.view.View.MeasureSpec;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnGenericMotionListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.view.Window;
-import android.view.WindowInsets;
 import android.view.WindowManager;
-import android.view.WindowMetrics;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -258,6 +248,7 @@ public class PlayerController implements View.OnTouchListener, OnGenericMotionLi
     private long                mLastTouchEventTime = -1;
     private View mPlayPauseTouchZone;
     private boolean mPlayPauseOnTouchActivated = false;
+    private static boolean mFullScreenWithCutout = true;
 
     private GestureDetector gestureDetector;
     private float currentBrightness;
@@ -571,7 +562,7 @@ public class PlayerController implements View.OnTouchListener, OnGenericMotionLi
         mLayoutWidth = layoutWidth;
         mLayoutHeight = layoutHeight;
         mSystemBarHeight = displayHeight - mLayoutHeight;
-        log.debug("CONFIG setSizes layout: " + mLayoutWidth + "x" + mLayoutHeight + " / display: " + displayWidth + "x" + displayHeight + ", systemBarHeight: " + mSystemBarHeight);
+        log.debug("CONFIG setSizes layout: {}x{} / display: {}x{}, systemBarHeight: {}", mLayoutWidth, mLayoutHeight, displayWidth, displayHeight, mSystemBarHeight);
         if (mControllerView != null) {
             log.debug("CONFIG setSizes, mControllerView != null, recreate whole layout");
             // size changed and maybe orientation too, recreate the whole layout
@@ -585,16 +576,14 @@ public class PlayerController implements View.OnTouchListener, OnGenericMotionLi
 
 
     private void attachWindow() {
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        if (mPreferences != null) mFullScreenWithCutout = mPreferences.getBoolean("enable_cutout_mode_short_edges", true);
 
-        log.debug("CONFIG attachWindow getStatusBarHeight=" + MiscUtils.getStatusBarHeight(mContext) +
-                ", getNavigationBarHeight=" + MiscUtils.getNavigationBarHeight(mContext) +
-                ", getActionBarHeight=" + MiscUtils.getActionBarHeight(mContext) +
-                ", getGestureAreaHeight=" + MiscUtils.getGestureAreaHeight(mContext) +
-                ", isGestureAreaDisplayed=" + MiscUtils.isGestureAreaDisplayed(mContext) +
-                ", ");
+        log.debug("CONFIG attachWindow getStatusBarHeight={}, getNavigationBarHeight={}, getActionBarHeight={}, getGestureAreaHeight={}, isGestureAreaDisplayed={}, ",
+                MiscUtils.getStatusBarHeight(mContext), MiscUtils.getNavigationBarHeight(mContext), MiscUtils.getActionBarHeight(mContext),
+                MiscUtils.getGestureAreaHeight(mContext), MiscUtils.isGestureAreaDisplayed(mContext));
 
-        if (mControllerView != null)
-            return;
+        if (mControllerView != null) return;
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         int layoutID=R.layout.player_controller;
         mControllerView = inflater.inflate(layoutID, null);
@@ -674,7 +663,7 @@ public class PlayerController implements View.OnTouchListener, OnGenericMotionLi
         //help overlay
         if(mContext instanceof PlayerActivity){
             if(((PlayerActivity)mContext).isPluggedOnTv()){
-                SharedPreferences mPreferences =  mContext.getSharedPreferences(MediaUtils.SHARED_PREFERENCES_NAME, mContext.MODE_PRIVATE);
+                mPreferences =  mContext.getSharedPreferences(MediaUtils.SHARED_PREFERENCES_NAME, mContext.MODE_PRIVATE);
                 if(!mPreferences.getBoolean(PLAYER_HELP_OVERLAY_KEY, false)){
                     showHelpOverlay(mControllerViewLeft);
                     if(mControllerViewRight!=null && UIMode!=VideoEffect.NORMAL_2D_MODE){
@@ -694,7 +683,7 @@ public class PlayerController implements View.OnTouchListener, OnGenericMotionLi
         MiscUtils.adjustViewLayoutForInsets(mContext, mRootView, mControllerView,"mControllerView",
                 mNavigationBarShowing, mSystemBarShowing, mActionBarShowing, mControlBarShowing, mIsNavBarOnBottom, mIsGestureAreaShowing, 0, 0,
                 true, true, true, true,
-                true, true, true, true);
+                true, true, true, true, false);
     }
 
     public static int getControlBarCurrentHeight() {

src/main/java/com/archos/mediacenter/video/player/SubtitleManager.java
@@ -19,28 +19,24 @@ import com.archos.mediacenter.video.utils.MiscUtils;
 import com.archos.medialib.Subtitle;
 import com.archos.medialib.Subtitle.SubtitleAlignment;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Insets;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
-import android.view.DisplayCutout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.ViewGroup.LayoutParams;
+import androidx.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 import androidx.core.content.ContextCompat;
 import androidx.core.text.HtmlCompat;
@@ -84,6 +80,8 @@ public class SubtitleManager {
     Surface                     mUiSurface;
     private boolean mForbidWindow ;
     DispSubtitleThread mDispSubtitleThread = null;
+    private static int mRoundCornerRadius = 0;
+    private static boolean mFullScreenWithCutout = true;
 
     public static final int SUBTITLE_TYPE_NONE = 0;
     public static final int SUBTITLE_TYPE_TEXT = 1;
@@ -568,8 +566,9 @@ public class SubtitleManager {
     }
 
     private void attachWindow() {
-        if (mSubtitleLayout != null)
-            return;
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        if (mPreferences != null) mFullScreenWithCutout = mPreferences.getBoolean("enable_cutout_mode_short_edges", true);
+        if (mSubtitleLayout != null) return;
         LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mSubtitleLayout = inflater.inflate(R.layout.subtitle_layout, mPlayerView, false);
         mSubtitleSpacer = (SubtitleSpacerView) mSubtitleLayout.findViewById(R.id.subtitle_spacer);
@@ -615,11 +614,14 @@ public class SubtitleManager {
     }
 
     private void adjustView() {
+        // strategy is videoView avoids cutout if not in fullscreen
+        // adjust subtitle text height (bottom/top) to avoid system bars and playerController bar only if text subtitle but not left/right
+        boolean avoidCutout = ! mFullScreenWithCutout;
         MiscUtils.adjustViewLayoutForInsets(mContext, mRootView, mSubtitleLayout, "mSubtitleLayout",
                 mNavigationBarShowing, mSystemBarShowing, mActionBarShowing, PlayerController.isControlBarShowing(), mIsNavBarOnBottom, mIsGestureAreaShowing,
                 (PlayerController.isControlBarShowing() ? PlayerController.getControlBarCurrentHeight() : 0), mSubtitleEvadedVPos,
-                false && ! mIsSubtitleGfx, true && ! mIsSubtitleGfx, false && ! mIsSubtitleGfx, true && ! mIsSubtitleGfx,
-                false && ! mIsSubtitleGfx, true && ! mIsSubtitleGfx, false && ! mIsSubtitleGfx, true && ! mIsSubtitleGfx);
+                false, ! mIsSubtitleGfx, false, ! mIsSubtitleGfx,
+                avoidCutout, avoidCutout, avoidCutout, avoidCutout, true);
     }
 
     private void detachWindow() {

commit 815d0e835a4457a304292a69ce36bf0b82a784c3

Fix external display video rendering in small format
Do not apply phone's display cutout metrics (notch, punch-hole, round edges)
to external HDMI displays. When HDMI is plugged, use full external display
dimensions without subtracting phone's cutout insets or applying cutout margins.

This fixes a regression introduced in commit 3c1bf8c1 (edge to edge sdk 35:
refactor code to manage cutout and system bars) where cutout avoidance logic
designed for the phone's display was incorrectly applied to external displays,
causing videos to render in a small box with large black borders around them.

The aspect ratio switching functionality is also restored since it now operates
on the correct base dimensions.

See https://github.com/nova-video-player/aos-AVP/issues/1596

src/main/java/com/archos/mediacenter/video/player/SurfaceController.java
@@ -273,8 +273,9 @@ public class SurfaceController {
         }
 
         // display width and height without cutout
-        int dcw = dw - mCutoutLeft - mCutoutRight;
-        int dch = dh - mCutoutTop - mCutoutBottom;
+        // When HDMI is plugged, do not apply phone's cutout metrics to external display
+        int dcw = mHdmiPlugged ? dw : (dw - mCutoutLeft - mCutoutRight);
+        int dch = mHdmiPlugged ? dh : (dh - mCutoutTop - mCutoutBottom);
         vw = mVideoWidth;
         vh = mVideoHeight;
 
@@ -358,8 +359,9 @@ public class SurfaceController {
         log.debug("CONFIG updateSurface: setLayoutParams({},{})", dcw, dch);
 
         // margins to avoid cutout
-        mMarginLeft = (int)((mCutoutLeft - mCutoutRight)/ 2.0f);
-        mMarginTop = (int)((mCutoutTop - mCutoutBottom)/ 2.0f);
+        // When HDMI is plugged, do not apply phone's cutout margins to external display
+        mMarginLeft = mHdmiPlugged ? 0 : (int)((mCutoutLeft - mCutoutRight)/ 2.0f);
+        mMarginTop = mHdmiPlugged ? 0 : (int)((mCutoutTop - mCutoutBottom)/ 2.0f);
 
         ViewGroup.LayoutParams lp = mView.getLayoutParams();
         if (lp instanceof ViewGroup.MarginLayoutParams marginParams) {