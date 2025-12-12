dfd884452e33a65612943bccb27c16f397fb33cd
Courville Software <software@courville.org>
2025-12-12 19:19:47 +0100

    NfoWriter: remove comment and uniformize logs

--- src/com/archos/mediascraper/NfoWriter.java
+++ src/com/archos/mediascraper/NfoWriter.java
@@ -290,10 +290,6 @@ public class NfoWriter {
         Uri exportTarget =  relocateNfoAppPublicDirForNfoJpgFiles(Uri.withAppendedPath(parent, videoName + NfoParser.CUSTOM_NFO_EXTENSION));
         try {
             FileEditor editor = FileEditorFactoryWithUpnp.getFileEditorForUrl(exportTarget, null);
-            // Delete existing file to avoid overwrite issue (end of previous content still there is the new content is shorter)
-            //if (editor.exists()) {
-            //    editor.delete();
-            //}
             BufferedWriter  writer = new BufferedWriter(new OutputStreamWriter(
                     editor.getOutputStream(), StringUtils.CHARSET_UTF8));
 
@@ -326,11 +322,7 @@ public class NfoWriter {
         Uri exportTarget =  relocateNfoAppPublicDirForNfoJpgFiles(Uri.withAppendedPath(parent, videoName + NfoParser.CUSTOM_NFO_EXTENSION));
         try {
             FileEditor editor = FileEditorFactoryWithUpnp.getFileEditorForUrl(exportTarget,null);
-            // Delete existing file to avoid overwrite issue (end of previous content still there is the new content is shorter)
-            //if (editor.exists()) {
-            //    editor.delete();
-            //}
-            log.trace("exportInternal: {}", video);
+            if (log.isTraceEnabled()) log.trace("exportInternal: {}", video);
             BufferedWriter  writer = new BufferedWriter(new OutputStreamWriter(
                     editor.getOutputStream(), StringUtils.CHARSET_UTF8));
 
@@ -363,13 +355,9 @@ public class NfoWriter {
         String showTitle = StringUtils.fileSystemEncode(tag.getTitle());
         // relocate uri for local files to writeable location to comply with API30
         Uri exportTarget =  relocateNfoAppPublicDirForNfoJpgFiles(Uri.withAppendedPath(parent, showTitle + NfoParser.CUSTOM_SHOW_NFO_EXTENSION));
-        log.debug("exportInternal: {} -> {}", video, exportTarget);
+        if (log.isDebugEnabled()) log.debug("exportInternal: {} -> {}", video, exportTarget);
         try {
             FileEditor editor = FileEditorFactoryWithUpnp.getFileEditorForUrl(exportTarget, null);
-            // Delete existing file to avoid overwrite issue (end of previous content still there is the new content is shorter)
-            //if (editor.exists()) {
-            //    editor.delete();
-            //}
             BufferedWriter  writer = new BufferedWriter(new OutputStreamWriter(
                     editor.getOutputStream(), StringUtils.CHARSET_UTF8));
 
