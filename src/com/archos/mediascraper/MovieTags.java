LeeroysHub/NoVa-Medialib

SCRAPING: Scrape from DB instead of TMDB if possible. * TV Shows and Movies both scrape. * Reduces TMDB queries to near 0 for multi-server * Speeds up by 50% Average 50% for TV and Movies
THIS WAS THE ONE IVE WANTED NOVA TO HAVE FOR YEARS!

src/org/leeroy/mediascraper/MovieTags.java
@@ -34,11 +34,14 @@ import org.leeroy.mediaprovider.video.VideoStore;
 import org.leeroy.mediascraper.ScraperImage.Type;
 
 
+import org.leeroy.mediascraper.themoviedb3.SearchMovieResult;
+import org.leeroy.mediascraper.themoviedb3.SearchParserResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MovieTags extends VideoTags {
@@ -210,7 +213,7 @@ public class MovieTags extends VideoTags {
             allOperations.add(cop.build());
         }
 
-        if (mCollectionId != -1) {
+        if (mCollectionId > 0) {
             // Check if this Movie Collection is already referenced in the scraperDB
             if (! isCollectionAlreadyKnown(mCollectionId, context)) {
                 //if (log.isDebugEnabled()) log.debug("save: collection {} does not exist, saving it", mCollectionId);
@@ -456,5 +459,44 @@ public class MovieTags extends VideoTags {
         image.setThumbUrl(ScraperImage.TMBT + path);
         image.generateFileNames(context);
         addDefaultBackdrop(image);
+    }
+
+    public static SearchMovieResult getMovieResultIfAlreadyKnown(Context context, String searchQuery, String year, Uri fileUri) {
+        ContentResolver contentResolver = context.getContentResolver();
+        String[] baseProjection = {ScraperStore.Movie.ID, ScraperStore.Movie.NAME}  ;
+
+        //Query the databbase.
+        if (year == null) year = "";
+        Cursor cursor = contentResolver.query(ScraperStore.Movie.URI.ALL ,
+                baseProjection,  "TRIM(REPLACE(REPLACE(REPLACE(REPLACE(name_movie, ':', ''), '-', ' '), '.', ' '), '& ', '')) LIKE ? AND " + ScraperStore.Movie.YEAR + " LIKE ?" , new String[] { "%" + searchQuery.trim() + "%", year + "%" }, null);
+
+        //Check cursor count and return if positive.
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                //Get the columns, so we can grab the data.
+                int idxVideoId = cursor.getColumnIndexOrThrow(ScraperStore.Movie.ID);
+                int idxName = cursor.getColumnIndexOrThrow(ScraperStore.Movie.NAME);
+
+                //Create a Search Result and populate it.
+                SearchMovieResult myResult = new SearchMovieResult();
+                SearchResult result = new SearchResult();
+                result.setMovie();
+                result.setFile(fileUri);
+                result.setId((int) cursor.getLong(idxVideoId));
+                result.setOriginalTitle( cursor.getString(idxName));
+                result.setTitle(cursor.getString(idxName));
+                result.setYear(year);
+                result.fromDB = true;
+
+                //Add the resut to a list, to pass back.
+                myResult.result = new LinkedList<>();
+                myResult.result.add(result);
+                myResult.status = ScrapeStatus.OKAY;
+                return myResult;
+            }
+
+            cursor.close();
+        }
+        return null;
     }
 }

src/org/leeroy/mediascraper/ShowTags.java
@@ -22,16 +22,18 @@ import android.content.Context;
 import android.content.OperationApplicationException;
 import android.content.ContentProviderOperation.Builder;
 import android.database.Cursor;
-import android.database.DatabaseUtils;
+//import android.database.DatabaseUtils;
 import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.RemoteException;
+//import android.provider.MediaStore;
 import android.text.TextUtils;
 
 import org.leeroy.mediaprovider.video.ScraperStore;
 import org.leeroy.mediascraper.ScraperImage.Type;
 
+import org.leeroy.mediascraper.themoviedb3.SearchShowResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,6 +43,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShowTags extends VideoTags {
@@ -635,5 +638,49 @@ public class ShowTags extends VideoTags {
             }
             cursor.close();
         }
+    }
+
+    public static SearchShowResult getEpisodeResultIfAlreadyKnown(Context context, String searchQuery, String season, String episode, Uri fileUri) {
+        ContentResolver contentResolver = context.getContentResolver();
+        String[] baseProjection = {ScraperStore.EpisodeShowCombined.SCRAPER_ID, ScraperStore.EpisodeShowCombined.SHOW_NAME,ScraperStore.EpisodeShowCombined.EPISODE_NAME}  ;
+
+        //No Slash on the Query, i AM NOT tmdb!
+        if (searchQuery.startsWith("/"))
+            searchQuery = searchQuery.substring(1);
+
+        //Query the databbase.
+        Uri mediaUri = ScraperStore.EpisodeShowCombined.URI.ALL;
+        Cursor cursor = contentResolver.query(mediaUri ,
+                baseProjection, "REPLACE(REPLACE(REPLACE(" + ScraperStore.Show.NAME+",'''',''),'.',''),':','') LIKE ? COLLATE NOCASE AND " + ScraperStore.EpisodeShowCombined.EPISODE_SEASON + " LIKE ? AND " + ScraperStore.EpisodeShowCombined.EPISODE_NUMBER + " LIKE ?" , new String[] { searchQuery+"%", season, episode },null);
+
+        //Check cursor count and return if positive.
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                //Get the columns, so we can grab the data.
+                int idxVideoId = cursor.getColumnIndexOrThrow(ScraperStore.EpisodeShowCombined.SCRAPER_ID);
+                int idxName = cursor.getColumnIndexOrThrow(ScraperStore.EpisodeShowCombined.EPISODE_NAME);
+
+                //Create a Search Result and populate it.
+                SearchShowResult myResult = new SearchShowResult();
+                SearchResult result = new SearchResult();
+                result.setTvShow();
+                result.setFile(fileUri);
+                result.setId((int) cursor.getLong(idxVideoId));
+                result.setOriginalTitle( cursor.getString(idxName));
+                result.setTitle(cursor.getString(idxName));
+                result.setOriginSearchEpisode(Integer.parseInt(episode));
+                result.setOriginSearchSeason(Integer.parseInt(season));
+                result.fromDB = true;
+
+                //Add the resut to a list, to pass back.
+                myResult.result = new LinkedList<>();
+                myResult.result.add(result);
+                myResult.status = ScrapeStatus.OKAY;
+                return myResult;
+            }
+
+            cursor.close();
+        }
+        return null;
     }
 }

src/org/leeroy/mediascraper/xml/MovieScraper3.java
@@ -15,11 +15,14 @@
 
 package org.leeroy.mediascraper.xml;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 
 import org.leeroy.medialib.R;
+import org.leeroy.mediaprovider.video.ScraperStore;
 import org.leeroy.mediascraper.MovieTags;
 import org.leeroy.mediascraper.ScrapeDetailResult;
 import org.leeroy.mediascraper.ScrapeSearchResult;
@@ -27,6 +30,7 @@ import org.leeroy.mediascraper.ScrapeStatus;
 import org.leeroy.mediascraper.Scraper;
 import org.leeroy.mediascraper.ScraperCache;
 import org.leeroy.mediascraper.SearchResult;
+import org.leeroy.mediascraper.TagsFactory;
 import org.leeroy.mediascraper.preprocess.MovieSearchInfo;
 import org.leeroy.mediascraper.preprocess.SearchInfo;
 import org.leeroy.mediascraper.themoviedb3.CollectionInfo;
@@ -43,6 +47,7 @@ import com.uwetrottmann.tmdb2.services.CollectionsService;
 import com.uwetrottmann.tmdb2.services.MoviesService;
 import com.uwetrottmann.tmdb2.services.SearchService;
 
+import org.leeroy.mediascraper.themoviedb3.SearchParserResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -168,10 +173,19 @@ public class MovieScraper3 extends BaseScraper2 {
             year = searchInfo.getYear();
         }
 
+        //Check the Database for this Movie, we may have scraped it already on a different URI.
+        SearchMovieResult searchResult = MovieTags.getMovieResultIfAlreadyKnown(mContext, searchQuery, year, searchInfo.getOriginalUri());
+        if (searchResult != null) {
+            for (SearchResult result : searchResult.result) {
+                result.setScraper(this);
+                result.setFile(searchInfo.getFile());
+            }
+            return new ScrapeSearchResult(searchResult.result, true, searchResult.status, searchResult.reason);
+        }
+
         //SEARCH TMDB FOR THE MOVIE!
         //If the Query is 5 words or more, and for as long as it is, remove a word
         //BUT! Only remove and try 3 times (Aussie Logic, backwards as fuck but works!)
-        SearchMovieResult searchResult = null;
         for (int i = 0; i < (searchInfo.aggressiveScan ? 4 : 1); i++ ){
             searchResult = SearchMovie2.search(searchQuery, language, year, maxItems, getSearchService(), adultScrape);
 
@@ -211,6 +225,12 @@ public class MovieScraper3 extends BaseScraper2 {
 
         long movieId = result.getId();
         Uri searchFile = result.getFile();
+
+        //If we got this result from the database, grab the tags from there and return them instead of going to TMDB.
+        if (result.fromDB){
+            MovieTags tag = TagsFactory.buildMovieTags(mContext, movieId);
+            return new ScrapeDetailResult(tag, true, null, ScrapeStatus.OKAY, null);
+        }
 
         // get base info
         MovieIdResult search = MovieId2.getBaseInfo(movieId, language, getMoviesService(), mContext);

src/org/leeroy/mediascraper/xml/ShowScraper4.java
@@ -36,6 +36,7 @@ import org.leeroy.mediascraper.ScraperImage;
 import org.leeroy.mediascraper.SearchResult;
 import org.leeroy.mediascraper.ShowTags;
 import org.leeroy.mediascraper.ShowUtils;
+import org.leeroy.mediascraper.TagsFactory;
 import org.leeroy.mediascraper.preprocess.SearchInfo;
 import org.leeroy.mediascraper.preprocess.TvShowSearchInfo;
 import org.leeroy.mediascraper.themoviedb3.MyTmdb;
@@ -61,13 +62,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-//import java.util.Locale;
 import java.util.Map;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.Future;
-//import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
 
@@ -144,7 +139,17 @@ public class ShowScraper4 extends BaseScraper2 {
         //if (log.isDebugEnabled()) log.debug("getMatches2: tvshow search:{} s:{} e:{}, maxItems={}, language={}",
                 //searchInfo.getShowName(), searchInfo.getSeason(), searchInfo.getEpisode(), maxItems, language);
 
-        SearchShowResult searchResult = SearchShow.search(searchInfo, language, maxItems, adultScrape,this, getTmdb());
+        //Check the Database for this Movie, we may have scraped it already on a different URI.
+        SearchShowResult searchResult = ShowTags.getEpisodeResultIfAlreadyKnown(mContext, searchInfo.getShowName(), String.valueOf(searchInfo.getSeason()), String.valueOf(searchInfo.getEpisode()),searchInfo.getOriginalUri());
+        if (searchResult != null) {
+            for (SearchResult result : searchResult.result) {
+                result.setScraper(this);
+                result.setFile(searchInfo.getFile());
+            }
+            return new ScrapeSearchResult(searchResult.result, false, searchResult.status, searchResult.reason);
+        }
+
+        searchResult = SearchShow.search(searchInfo, language, maxItems, adultScrape,this, getTmdb());
         //if (log.isDebugEnabled()) if (searchResult.result.size() > 0) log.debug("getMatches2: match found {} id {}", searchResult.result.get(0).getTitle(), searchResult.result.get(0).getId());
         return new ScrapeSearchResult(searchResult.result, false, searchResult.status, searchResult.reason);
     }
@@ -180,9 +185,15 @@ public class ShowScraper4 extends BaseScraper2 {
 
         //Save the Show ID and Cleanup the Name now for the Cache Keys.
         int showId = result.getId();
-        String cleanShowName = ShowUtils.cleanUpName(result.getOriginalTitle().toLowerCase());
+
+        //If we got this result from the database, grab the tags from there and return them instead of going to TMDB.
+        if (result.fromDB){
+            EpisodeTags tag = TagsFactory.buildEpisodeTags(mContext, showId);
+            return new ScrapeDetailResult(tag, false, null, ScrapeStatus.OKAY, null);
+        }
 
         //Build the Show and Episode keys.
+        String cleanShowName = ShowUtils.cleanUpName(result.getOriginalTitle().toLowerCase());
         String showKey = cleanShowName + "|" + resultLanguage;
         String seasonKey =  cleanShowName + "|" + (season != -1 ? season : "")  + "|all|" + resultLanguage;
         String episodeKey = showId + "|"  + (season != -1 ? season : "") + "|" +  (episode != -1 ? episode : "") + "|" + resultLanguage;

Joy4orce/Joy4_nova

src/com/archos/mediascraper/MovieTags.java
@@ -34,11 +34,13 @@ import com.archos.mediaprovider.video.VideoStore;
 import com.archos.mediascraper.ScraperImage.Type;
 
 
+import com.archos.mediascraper.themoviedb3.SearchMovieResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MovieTags extends VideoTags {
@@ -210,7 +212,7 @@ public class MovieTags extends VideoTags {
             allOperations.add(cop.build());
         }
 
-        if (mCollectionId != -1) {
+        if (mCollectionId > 0) {
             // Check if this Movie Collection is already referenced in the scraperDB
             if (! isCollectionAlreadyKnown(mCollectionId, context)) {
                 if (log.isDebugEnabled()) log.debug("save: collection {} does not exist, saving it", mCollectionId);
@@ -456,5 +458,44 @@ public class MovieTags extends VideoTags {
         image.setThumbUrl(ScraperImage.TMBT + path);
         image.generateFileNames(context);
         addDefaultBackdrop(image);
+    }
+
+    public static SearchMovieResult getMovieResultIfAlreadyKnown(Context context, String searchQuery, String year, Uri fileUri) {
+        ContentResolver contentResolver = context.getContentResolver();
+        String[] baseProjection = {ScraperStore.Movie.ID, ScraperStore.Movie.NAME}  ;
+
+        //Query the databbase.
+        if (year == null) year = "";
+        Cursor cursor = contentResolver.query(ScraperStore.Movie.URI.ALL ,
+                baseProjection,  "TRIM(REPLACE(REPLACE(REPLACE(REPLACE(name_movie, ':', ''), '-', ' '), '.', ' '), '& ', '')) LIKE ? AND " + ScraperStore.Movie.YEAR + " LIKE ?" , new String[] { "%" + searchQuery.trim() + "%", year + "%" }, null);
+
+        //Check cursor count and return if positive.
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                //Get the columns, so we can grab the data.
+                int idxVideoId = cursor.getColumnIndexOrThrow(ScraperStore.Movie.ID);
+                int idxName = cursor.getColumnIndexOrThrow(ScraperStore.Movie.NAME);
+
+                //Create a Search Result and populate it.
+                SearchMovieResult myResult = new SearchMovieResult();
+                SearchResult result = new SearchResult();
+                result.setMovie();
+                result.setFile(fileUri);
+                result.setId((int) cursor.getLong(idxVideoId));
+                result.setOriginalTitle( cursor.getString(idxName));
+                result.setTitle(cursor.getString(idxName));
+                result.setYear(year);
+                result.fromDB = true;
+
+                //Add the resut to a list, to pass back.
+                myResult.result = new LinkedList<>();
+                myResult.result.add(result);
+                myResult.status = ScrapeStatus.OKAY;
+                return myResult;
+            }
+
+            cursor.close();
+        }
+        return null;
     }
 }

src/com/archos/mediascraper/ShowTags.java
@@ -27,11 +27,13 @@ import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.RemoteException;
+//import android.provider.MediaStore;
 import android.text.TextUtils;
 
 import com.archos.mediaprovider.video.ScraperStore;
 import com.archos.mediascraper.ScraperImage.Type;
 
+import com.archos.mediascraper.themoviedb3.SearchShowResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,6 +43,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShowTags extends VideoTags {
@@ -635,5 +638,49 @@ public class ShowTags extends VideoTags {
             }
             cursor.close();
         }
+    }
+
+    public static SearchShowResult getEpisodeResultIfAlreadyKnown(Context context, String searchQuery, String season, String episode, Uri fileUri) {
+        ContentResolver contentResolver = context.getContentResolver();
+        String[] baseProjection = {ScraperStore.EpisodeShowCombined.SCRAPER_ID, ScraperStore.EpisodeShowCombined.SHOW_NAME,ScraperStore.EpisodeShowCombined.EPISODE_NAME}  ;
+
+        //No Slash on the Query, i AM NOT tmdb!
+        if (searchQuery.startsWith("/"))
+            searchQuery = searchQuery.substring(1);
+
+        //Query the databbase.
+        Uri mediaUri = ScraperStore.EpisodeShowCombined.URI.ALL;
+        Cursor cursor = contentResolver.query(mediaUri ,
+                baseProjection, "REPLACE(REPLACE(REPLACE(" + ScraperStore.Show.NAME+",'''',''),'.',''),':','') LIKE ? COLLATE NOCASE AND " + ScraperStore.EpisodeShowCombined.EPISODE_SEASON + " LIKE ? AND " + ScraperStore.EpisodeShowCombined.EPISODE_NUMBER + " LIKE ?" , new String[] { searchQuery+"%", season, episode },null);
+
+        //Check cursor count and return if positive.
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                //Get the columns, so we can grab the data.
+                int idxVideoId = cursor.getColumnIndexOrThrow(ScraperStore.EpisodeShowCombined.SCRAPER_ID);
+                int idxName = cursor.getColumnIndexOrThrow(ScraperStore.EpisodeShowCombined.EPISODE_NAME);
+
+                //Create a Search Result and populate it.
+                SearchShowResult myResult = new SearchShowResult();
+                SearchResult result = new SearchResult();
+                result.setTvShow();
+                result.setFile(fileUri);
+                result.setId((int) cursor.getLong(idxVideoId));
+                result.setOriginalTitle( cursor.getString(idxName));
+                result.setTitle(cursor.getString(idxName));
+                result.setOriginSearchEpisode(Integer.parseInt(episode));
+                result.setOriginSearchSeason(Integer.parseInt(season));
+                result.fromDB = true;
+
+                //Add the resut to a list, to pass back.
+                myResult.result = new LinkedList<>();
+                myResult.result.add(result);
+                myResult.status = ScrapeStatus.OKAY;
+                return myResult;
+            }
+
+            cursor.close();
+        }
+        return null;
     }
 }

src/com/archos/mediascraper/xml/MovieScraper3.java
@@ -27,6 +27,7 @@ import com.archos.mediascraper.ScrapeStatus;
 import com.archos.mediascraper.Scraper;
 import com.archos.mediascraper.ScraperCache;
 import com.archos.mediascraper.SearchResult;
+import com.archos.mediascraper.TagsFactory;
 import com.archos.mediascraper.preprocess.MovieSearchInfo;
 import com.archos.mediascraper.preprocess.SearchInfo;
 import com.archos.mediascraper.themoviedb3.CollectionInfo;
@@ -39,6 +40,7 @@ import com.archos.mediascraper.themoviedb3.MovieIdResult;
 import com.archos.mediascraper.themoviedb3.MyTmdb;
 import com.archos.mediascraper.themoviedb3.SearchMovie2;
 import com.archos.mediascraper.themoviedb3.SearchMovieResult;
+
 import com.uwetrottmann.tmdb2.services.CollectionsService;
 import com.uwetrottmann.tmdb2.services.MoviesService;
 import com.uwetrottmann.tmdb2.services.SearchService;
@@ -139,10 +141,19 @@ public class MovieScraper3 extends BaseScraper2 {
 
         String reversed = new StringBuilder(searchQuery).reverse().toString();
 
+        //Check the Database for this Movie, we may have scraped it already on a different URI.
+        SearchMovieResult searchResult = MovieTags.getMovieResultIfAlreadyKnown(mContext, searchQuery, searchInfo.getYear(), searchInfo.getOriginalUri());
+        if (searchResult != null) {
+            for (SearchResult result : searchResult.result) {
+                result.setScraper(this);
+                result.setFile(searchInfo.getFile());
+            }
+            return new ScrapeSearchResult(searchResult.result, true, searchResult.status, searchResult.reason);
+        }
+
         //SEARCH TMDB FOR THE MOVIE!
         //If the Query is 5 words or more, and for as long as it is, remove a word
         //BUT! Only remove and try 3 times (Aussie Logic, backwards as fuck but works!)
-        SearchMovieResult searchResult = null;
         for (int i = 0; i < (searchInfo.aggressiveScan ? 4 : 1); i++ ){
             searchResult = SearchMovie2.search(searchQuery, language, searchInfo.getYear(), maxItems, getSearchService(), adultScrape);
             //Check result and try again if we need to.
@@ -181,6 +192,12 @@ public class MovieScraper3 extends BaseScraper2 {
 
         long movieId = result.getId();
         Uri searchFile = result.getFile();
+
+        //If we got this result from the database, grab the tags from there and return them instead of going to TMDB.
+        if (result.fromDB){
+            MovieTags tag = TagsFactory.buildMovieTags(mContext, movieId);
+            return new ScrapeDetailResult(tag, true, null, ScrapeStatus.OKAY, null);
+        }
 
         // get base info
         MovieIdResult search = MovieId2.getBaseInfo(movieId, language, getMoviesService(), mContext);

src/com/archos/mediascraper/xml/ShowScraper4.java
@@ -34,6 +34,7 @@ import com.archos.mediascraper.Scraper;
 import com.archos.mediascraper.ScraperCache;
 import com.archos.mediascraper.ScraperImage;
 import com.archos.mediascraper.SearchResult;
+import com.archos.mediascraper.TagsFactory;
 import com.archos.mediascraper.ShowTags;
 import com.archos.mediascraper.ShowUtils;
 import com.archos.mediascraper.preprocess.SearchInfo;
@@ -61,7 +62,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
+
 import java.util.Map;
 
 import okhttp3.Cache;
@@ -139,7 +140,17 @@ public class ShowScraper4 extends BaseScraper2 {
         if (log.isDebugEnabled()) log.debug("getMatches2: tvshow search:{} s:{} e:{}, maxItems={}, language={}",
                 searchInfo.getShowName(), searchInfo.getSeason(), searchInfo.getEpisode(), maxItems, language);
 
-        SearchShowResult searchResult = SearchShow.search(searchInfo, language, maxItems, adultScrape,this, getTmdb());
+        //Check the Database for this Movie, we may have scraped it already on a different URI.
+        SearchShowResult searchResult = ShowTags.getEpisodeResultIfAlreadyKnown(mContext, searchInfo.getShowName(), String.valueOf(searchInfo.getSeason()), String.valueOf(searchInfo.getEpisode()),searchInfo.getOriginalUri());
+        if (searchResult != null) {
+            for (SearchResult result : searchResult.result) {
+                result.setScraper(this);
+                result.setFile(searchInfo.getFile());
+            }
+            return new ScrapeSearchResult(searchResult.result, false, searchResult.status, searchResult.reason);
+        }
+
+        searchResult = SearchShow.search(searchInfo, language, maxItems, adultScrape,this, getTmdb());
         if (log.isDebugEnabled()) if (searchResult.result.size() > 0) log.debug("getMatches2: match found {} id {}", searchResult.result.get(0).getTitle(), searchResult.result.get(0).getId());
         return new ScrapeSearchResult(searchResult.result, false, searchResult.status, searchResult.reason);
     }
@@ -170,9 +181,15 @@ public class ShowScraper4 extends BaseScraper2 {
 
         //Save the Show ID and Cleanup the Name now for the Cache Keys.
         int showId = result.getId();
-        String cleanShowName = ShowUtils.cleanUpName(result.getOriginalTitle().toLowerCase());
+
+        //If we got this result from the database, grab the tags from there and return them instead of going to TMDB.
+        if (result.fromDB){
+            EpisodeTags tag = TagsFactory.buildEpisodeTags(mContext, showId);
+            return new ScrapeDetailResult(tag, false, null, ScrapeStatus.OKAY, null);
+        }
 
         //Build the Show and Episode keys.
+        String cleanShowName = ShowUtils.cleanUpName(result.getOriginalTitle().toLowerCase());
         String showKey = cleanShowName + "|" + resultLanguage;
         String seasonKey =  cleanShowName + "|" + (season != -1 ? season : "")  + "|all|" + resultLanguage;
         String episodeKey = showId + "|"  + (season != -1 ? season : "") + "|" +  (episode != -1 ? episode : "") + "|" + resultLanguage;